6c0403970abe149d75c72731e2d3427550f0690c
johnpatrick254 <jpattrick538@gmail.com>
2024-04-14 03:46:58 +0300

    style: add error utility

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors:{
-        primary:'#7856ff'
+        primary:'#7856ff',
+        error: "#D2042D"
       }
     },
   },
