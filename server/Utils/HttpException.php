6130972757f55a561db86e966515ab53a3bb2db0
johnpatrick254 <jpattrick538@gmail.com>
2024-05-08 20:43:55 +0300

    feat: add array support for messages

--- server/Utils/HttpException.php
+++ server/Utils/HttpException.php
@@ -5,7 +5,7 @@ namespace App\Utils;
 class HttpException
 {
 
-    public static function handleException(int $code, string $message)
+    public static function handleException(int $code, $message)
     {
         http_response_code($code);
         return print(json_encode(["error" => $message]));
