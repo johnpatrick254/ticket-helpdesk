4f150620143562e190246e82234c377e0d4407b3
johnpatrick254 <jpattrick538@gmail.com>
2024-05-10 16:46:06 +0300

    feat: add migrations

--- server/Config/migrations.php
+++ server/Config/migrations.php
@@ -10,12 +10,38 @@ function runMigration()
     $connection->beginTransaction();
     try {
         //CLEAR SCHEMA IF IT EXISTS
-        $responseResetQuery = $connection->prepare("DROP TABLE IF EXISTS response");
+        $responseResetQuery = $connection->prepare("DROP TABLE IF EXISTS responses");
         $statement = $responseResetQuery->execute();
         $ticketResetQuery = $connection->prepare("DROP TABLE IF EXISTS tickets");
         $statement = $ticketResetQuery->execute();
         $userResetQuery = $connection->prepare("DROP TABLE IF EXISTS users;");
         $statement = $userResetQuery->execute();
+
+        //FUNCTIONS 
+        $triggerTicketSetTimeStamp = $connection->prepare("
+       CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+       RETURNS TRIGGER AS $$
+       BEGIN
+        NEW.last_updated = Now();
+        IF NEW.priority = 'completed' THEN
+        NEW.completed_at = Now();
+        END IF; 
+        RETURN NEW;
+       END;
+       $$ LANGUAGE plpgsql;
+       ")->execute();
+
+        $triggerResponseSetTimeStamp = $connection->prepare("
+       CREATE OR REPLACE FUNCTION trigger_response_set_timestamp()
+       RETURNS TRIGGER AS $$
+       BEGIN
+        UPDATE tickets
+        SET last_updated = Now()
+        WHERE id = NEW.ticket_id;
+        RETURN NEW;
+       END;
+       $$ LANGUAGE plpgsql;
+       ")->execute();
 
         //////////////////
         ///USER SCHEMA///
@@ -25,6 +51,8 @@ function runMigration()
         CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         email VARCHAR(150) UNIQUE NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
         password VARCHAR(50) NOT NULL,
         role VARCHAR(50) DEFAULT 'user'
         );
@@ -43,25 +71,48 @@ function runMigration()
         title VARCHAR(300) NOT NULL,
         body  TEXT NOT NULL,
         priority VARCHAR(5) DEFAULT 'low',
-        user_id int REFERENCES users(id)
+        user_id int REFERENCES users(id),
+        date_created TIMESTAMP DEFAULT NOW(),
+        last_updated TIMESTAMP DEFAULT NOW(),
+        completed_at TIMESTAMP 
         );
         ";
         $statement = $connection->prepare($ticketSchemaQuery);
         $statement->execute();
+
+        //Ticket Trigger
+
+        $ticketTrigger = $connection->prepare("
+        CREATE TRIGGER set_timestamps
+        BEFORE UPDATE ON tickets
+        FOR EACH ROW
+        EXECUTE PROCEDURE  trigger_set_timestamp();
+        ")->execute();
+
         ///////////////////
         ///Response SCHEMA///
         //////////////////
 
         $responseQuery = "
-        CREATE TABLE response(
+        CREATE TABLE responses(
             id SERIAL PRIMARY KEY,
             content TEXT NOT NULL,
             sender_id int REFERENCES users(id),
-            ticket_id  int REFERENCES tickets(id)
+            ticket_id  int REFERENCES tickets(id),
+            date_created TIMESTAMP DEFAULT NOW()
         );
         ";
         $statement = $connection->prepare($responseQuery);
         $statement->execute();
+
+        //Response Trigger
+
+        $responseTrigger = $connection->prepare('
+        CREATE TRIGGER set_response_last_updated
+        BEFORE UPDATE ON responses
+        FOR EACH ROW
+        EXECUTE PROCEDURE trigger_response_set_timestamp();
+        ')->execute();
 
         //COMMIT TRANSACTION AND END MIGRATION
         $connection->commit();
