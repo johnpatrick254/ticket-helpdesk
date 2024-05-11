9e91a97a4f37dec7ae27f4514934e40076809a09
johnpatrick254 <jpattrick538@gmail.com>
2024-05-11 19:27:54 +0300

    feat: configure middleware methods

--- server/Routes/Router.php
+++ server/Routes/Router.php
@@ -1,13 +1,22 @@
 <?php
 namespace App\Routes;
+
 class Router
 {
-      public static function handleResource(string $path,BaseRoute $class){
-          $url = array_filter(explode('/', $_SERVER['REQUEST_URI']));
-          $targetPath = array_filter(explode('/', $path));
-          if($targetPath[1] === $url[2]) {
-               return $class::handleRoute($url);
-          }
-          
+    public static function handleResource(string $path, BaseRoute $class)
+    {
+        $url = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+        $targetPath = array_filter(explode('/', $path));
+        if ($targetPath[1] === $url[2]) {
+            return $class::handleRoute($url);
+        }
     }
+    public static function withMiddleware(array $middlewares, string $path, BaseRoute $class)
+    {
+        foreach ($middlewares as $middleware) {
+            $middleware::activate();
+        }
+        return self::handleResource($path, $class);
+    }
+
 }
