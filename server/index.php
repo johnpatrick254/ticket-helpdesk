8310fc3ea93d17e09a24033924b12698acfb3222
johnpatrick254 <jpattrick538@gmail.com>
2024-05-12 23:45:44 +0300

    feat: add ticket route handler and initiate db

--- server/index.php
+++ server/index.php
@@ -1,6 +1,9 @@
 <?php
+use App\Middlewares\AuthMiddleware;
+use App\Routes\TicketRoute;
 require 'vendor/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 use App\Routes\AuthRoute;
 use App\Routes\Router;
 Router::handleResource('/auth',new AuthRoute);
+Router::withMiddleware([AuthMiddleware::class],'/tickets',new TicketRoute);
