219b02312667e36b06e048c7585a03da5bebd8eb
johnpatrick254 <jpattrick538@gmail.com>
2024-05-22 14:50:49 +0300

    feat: add auth layer

--- client/src/app/components/Nav.tsx
+++ client/src/app/components/Nav.tsx
@@ -1,29 +1,17 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import BrandLogo from '../../../public/brandlogo.svg'
 import Image from 'next/image'
 import Logout from '../tickets/shared/logout'
-import { redirect } from 'next/navigation'
+import UserMessage from './usermessage'
+
 
 
 export default function Nav() {
-   const getToken = () => {
-        if (typeof window !== 'undefined') {
-            const auth = localStorage.getItem('auth');
-            if (!auth) return null;
-            const { token, user } = JSON.parse(auth);
-            const expTime = new Date(user.exp).getHours();
-            const now = new Date().getHours()
-            if ((expTime - now) < 0) return null;
-            return { token: `Bearer ${token}`, user };
-        }
-    }
-    const auth = getToken()
-    if (!auth) {
-        redirect('/login')
-    }
+
+
     return (
-        <nav className="flex w-full space-x-3">
+        <nav className="flex w-full items-center align-middle space-x-3">
             <Image
                 src={BrandLogo}
                 width={70}
@@ -32,11 +20,9 @@ export default function Nav() {
             <h1>Help-desk</h1>
             <Link href='/'>Dashboard</Link>
             <Link href='/tickets'>Tickets</Link>
-            <div className='ml-auto'>
-                <span>
-                    Hello {auth.user.first_name}, Role: <span className='pill bg-primary'>{auth.user.role}</span>
-                </span>
-            </div>
+            <Suspense>
+                <UserMessage />
+            </Suspense>
             <Logout />
         </nav>
     )
--- client/src/app/tickets/shared/logout.tsx
+++ client/src/app/tickets/shared/logout.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import { clearUser } from "@/app/_services/api/AuthSlice";
+import { useAppSelector } from "@/app/_utills/hooks/redux-hooks";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux"
 
 export default function Logout() {
     const dispatch = useDispatch();
     const router = useRouter();
-
+    const auth = useAppSelector(state => state.auth)
+    if (!auth.user) {
+        router.push('/login')
+    }
     const handleLogOut = () => {
         if (typeof window !== 'undefined'){
             dispatch(clearUser());
