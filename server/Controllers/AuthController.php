47ba52a6eb0d214365bc39077e0a6f131484ea29
johnpatrick254 <jpattrick538@gmail.com>
2024-05-25 23:14:52 +0300

    fix: update validation logic

--- server/Controllers/AuthController.php
+++ server/Controllers/AuthController.php
@@ -13,19 +13,20 @@ class AuthController extends BaseController
     public static function handleCollectionRequest(array $url)
     {
         $data = (array) json_decode(file_get_contents('php://input'));
-        if (isset($url[3]) && ($url[3] === 'login') && count($url) < 4) {
+
+        if (isset($url[3]) && ($url[3] === 'login') && count($url) < 3) {
             return self::handleSignIn($data);
         }
-        if (isset($url[3]) && ($url[3] === 'register') && count($url) < 4) {
+        if (isset($url[3]) && ($url[3] === 'register') && count($url) < 3) {
             return self::handleSignUp($data);
         }
         return HttpException::handleException(404, 'Not Found');
     }
-
+    
     public static function handleResourceRequest(array $url)
     {
-        if (isset($url[3]) && ($url[3] === 'verify') && count($url) < 4) {
-            return self::withMiddleware([AuthMiddleware::class], [self::class, 'handleVerifySignIn']);
+        if (isset($url[3]) && ($url[3] === 'verify') && count($url) < 3) {
+            return self::withMiddleware(['AuthMiddleware'], [self::class, 'handleVerifySignIn']);
         }
         return HttpException::handleException(404, 'Not Found');
     }
@@ -36,6 +37,7 @@ class AuthController extends BaseController
         $user = UserModel::findUser($validatedData['email']);
         if ($user && $user['password'] === $validatedData['password']) {
             unset($user['password']);
+            http_response_code(200);
             echo json_encode(['token' => JWT::encode($user)]);
         } else {
             return HttpException::handleException(401, 'email or password is incorrect');
@@ -52,6 +54,5 @@ class AuthController extends BaseController
         $validatedData = $registerDTO->validate();
         $user = new UserModel($validatedData['first_name'], $validatedData['last_name'], $validatedData['email'], $validatedData['password'], $validatedData['role']);
         $user->save();
-        http_response_code(201);
     }
 }
