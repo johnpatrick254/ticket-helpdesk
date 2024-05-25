c3e45474b71b6ebd0c8db287b322d70fd1f141de
johnpatrick254 <jpattrick538@gmail.com>
2024-05-25 23:08:10 +0300

    chore: log server status

--- client/src/app/tickets/shared/logout.tsx
+++ client/src/app/tickets/shared/logout.tsx
@@ -1,15 +1,20 @@
 "use client"
 
 import { clearUser } from "@/app/_services/api/AuthSlice";
-import { useAppSelector } from "@/app/_utills/hooks/redux-hooks";
+import { getToken } from "@/app/_utills/auth";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux"
 
 export default function Logout() {
     const dispatch = useDispatch();
     const router = useRouter();
-    const auth = useAppSelector(state => state.auth)
-    if (!auth.user) {
+    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+    axios.get(`${BASE_URL}/health`).then(res => {
+        console.log(`\n ********** server status:${res.data.status} ************\n`)
+    })
+    const auth = getToken();
+    if (!auth) {
         router.push('/login')
     }
     const handleLogOut = () => {
