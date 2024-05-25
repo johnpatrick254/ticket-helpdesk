9dbf13c0a96bc83affe78ac9985a53e7ab66d695
johnpatrick254 <jpattrick538@gmail.com>
2024-05-25 23:18:19 +0300

    feat:configure docker and nginx for local development

--- server/index.php
+++ server/index.php
@@ -1,6 +1,7 @@
 <?php
 use App\Config\Database;
 use App\Middlewares\AuthMiddleware;
+use App\Routes\HealthRoute;
 use App\Routes\ResponseRoute;
 use App\Routes\Router;
 use App\Routes\TicketRoute;
@@ -8,10 +9,18 @@ use App\Routes\AuthRoute;
 
 require 'vendor/autoload.php';
 
-header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *'); // Replace * with the origin you want to allow
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    header('Access-Control-Allow-Methods: POST,PUT,GET,DELETE,OPTIONS'); // Add any other methods your server supports
+    header('Access-Control-Allow-Headers: Content-Type,Authorization'); // Add any other headers your server expects
+    exit;
+}
 
 Database::getDatabaseInstance();
-
+Router::handleResource('/health', new HealthRoute);
 Router::handleResource('/auth', new AuthRoute);
 Router::withMiddleware([AuthMiddleware::class], '/tickets', new TicketRoute);
 Router::withMiddleware([AuthMiddleware::class], '/responses', new ResponseRoute);
