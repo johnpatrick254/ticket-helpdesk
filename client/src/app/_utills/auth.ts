8d0707d7f607ce0efdc1eb8c934c93bf2588f8bd
John Patrick <jpattrick538@gmail.com>
2024-06-05 21:50:35 +0300

    fix: faulty token expiry logic

--- client/src/app/_utills/auth.ts
+++ client/src/app/_utills/auth.ts
@@ -1,14 +1,15 @@
 "use client"
 
 export const getToken = () => {
-  if (typeof window !== 'undefined'){
-      const auth = localStorage.getItem('auth');
-      if (!auth) return null;
-      const { token, user } = JSON.parse(auth);
-      const expTime = new Date(user.exp).getHours();
-      const now = new Date().getHours()
-      if ((expTime - now) < 0) return null;
-      return { token: `Bearer ${token}`, user };
+  if (typeof window !== 'undefined') {
+    const auth = localStorage.getItem('auth');
+    if (!auth) return null;
+
+    const { token, user } = JSON.parse(auth);
+    const expTime = user.exp;
+    const now = new Date().getTime();
+    const threeHoursInMillis = 3 * 60 * 60 * 1000;
+    if ((expTime - now) > threeHoursInMillis) return null;
+    return { token: `Bearer ${token}`, user };
   }
 }
-
